3010950c57bbb0f95f6d3d6505e7eb1d82b90e2e
AXAz0r <thealeksaradovic@gmail.com>
2018-01-05 01:23:33 +0100

    Fixed Websocket and REST Colide

--- lucia/core/lucia.py
+++ lucia/core/lucia.py
@@ -9,20 +9,20 @@ from lucia.core.nodes.database import Database
 from lucia.core.nodes.loader import EndpointLoader
 
 
-class LuciaCore(flask.Flask):
+class LuciaCore(object):
     def __init__(self):
-        super().__init__(__name__)
+        self.flask = flask.Flask(__name__)
         self.start = arrow.utcnow()
         self.cfg = Configuration()
         self.db = Database(self)
         # noinspection PyTypeChecker
-        self.rest = flask_restful.Api(self)
-        self.webs = flask_socketio.SocketIO(self)
+        self.rest = flask_restful.Api(self.flask)
+        self.webs = flask_socketio.SocketIO(self.flask)
         self.loader = EndpointLoader(self)
 
-    def run(self, *args):
+    def run(self):
         print('--------------------------------')
         print('Starting Up Lucia\'s Endpoint!')
         print('--------------------------------')
-        flask_cors.CORS(self, origins=self.cfg.app.cors)
-        super().run(self.cfg.app.host, self.cfg.app.port, self.cfg.app.debug)
+        flask_cors.CORS(self.flask, origins=self.cfg.app.cors)
+        self.webs.run(self.flask, self.cfg.app.host, self.cfg.app.port)
--- lucia/endpoints/webs/sigma/stats/stats.py
+++ lucia/endpoints/webs/sigma/stats/stats.py
@@ -12,20 +12,11 @@ def load_webs_endpoint(core):
             super().__init__(namespace)
             self.core = core
 
-        @staticmethod
-        def on_connect():
-            print('Client connected.')
-
-        @staticmethod
-        def on_disconnect():
-            print('Client disconnected.')
-
         def on_test(self, data):
             out_data = json.dumps({
                 'now': arrow.utcnow().float_timestamp,
                 'start': self.core.start.float_timestamp
             })
             emit('current_time', out_data)
-            print(data)
 
     return SigmaStatistics, location
