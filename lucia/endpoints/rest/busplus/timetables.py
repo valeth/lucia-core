2d2b22e4c72026aac20467d60d99bc0325a78f32
AXAz0r <thealeksaradovic@gmail.com>
2018-04-10 00:40:31 +0200

    Fix None Check

--- lucia/endpoints/rest/busplus/timetables.py
+++ lucia/endpoints/rest/busplus/timetables.py
@@ -25,9 +25,10 @@ def load_rest_endpoint(core):
             line_url = None
             line_buttons = root.cssselect('.vc_btn3')
             for line_button in line_buttons:
-                if line_button.text.lower() == line_number.lower():
-                    line_url = f'{url_base}{line_button.attrib.get("href")}'
-                    break
+                if line_button:
+                    if line_button.text.lower() == line_number.lower():
+                        line_url = f'{url_base}{line_button.attrib.get("href")}'
+                        break
             return line_url
 
         @staticmethod
