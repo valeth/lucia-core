8b91c9bdcd04c1ba1471e8bd106d64ec1abc6043
AXAz0r <thealeksaradovic@gmail.com>
2018-04-23 21:42:31 +0200

    Unused Attribs Cleaned

--- lucia/core/lucia.py
+++ lucia/core/lucia.py
@@ -1,13 +1,10 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
 import arrow
-import discord
 import flask
-# import flask_socketio
+import flask_socketio
 import flask_cors
 import flask_restful
-# import flask_socketio
 
 from lucia.core.nodes.config import Configuration
 from lucia.core.nodes.database import Database
@@ -20,11 +17,10 @@ class LuciaCore(object):
         self.start = arrow.utcnow()
         self.cfg = Configuration()
         self.db = Database(self)
-        self.bot = discord.Client()
         self.loop = asyncio.get_event_loop()
         # noinspection PyTypeChecker
         self.rest = flask_restful.Api(self.flask)
-        # self.webs = flask_socketio.SocketIO(self.flask)
+        self.webs = flask_socketio.SocketIO(self.flask)
         self.loader = EndpointLoader(self)
 
     def run(self):
@@ -32,5 +28,4 @@ class LuciaCore(object):
         print('Starting Up Lucia\'s Endpoint!')
         print('--------------------------------')
         flask_cors.CORS(self.flask, origins=self.cfg.app.cors)
-        self.loop.run_in_executor(ThreadPoolExecutor(), self.bot.start(self.cfg.app.token))
         self.flask.run(self.cfg.app.host, self.cfg.app.port, debug=self.cfg.app.debug)
