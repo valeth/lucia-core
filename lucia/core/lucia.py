bbda1c371dd862a8415b86ebe750182871daba9d
AXAz0r <thealeksaradovic@gmail.com>
2018-05-29 22:10:12 +0200

    Core Inherits Flask

--- lucia/core/lucia.py
+++ lucia/core/lucia.py
@@ -11,24 +11,21 @@ from lucia.core.nodes.database import Database
 from lucia.core.nodes.loader import EndpointLoader
 
 
-class LuciaCore(object):
+class LuciaCore(flask.Flask):
     def __init__(self):
-        self.flask = flask.Flask(__name__)
+        super().__init__(__name__)
         self.start = arrow.utcnow()
         self.cfg = Configuration()
         self.db = Database(self)
         self.loop = asyncio.get_event_loop()
         # noinspection PyTypeChecker
-        self.rest = flask_restful.Api(self.flask)
-        self.webs = flask_socketio.SocketIO(self.flask)
+        self.rest = flask_restful.Api(self)
+        self.webs = flask_socketio.SocketIO(self)
         self.loader = EndpointLoader(self)
 
-    def __call__(self, *args, **kwargs):
-        self.flask.__call__(*args, **kwargs)
-
-    def run(self):
+    def run(self, *args):
         print('--------------------------------')
         print('Starting Up Lucia\'s Endpoint!')
         print('--------------------------------')
-        flask_cors.CORS(self.flask, origins=self.cfg.app.cors)
-        self.flask.run(self.cfg.app.host, self.cfg.app.port, debug=self.cfg.app.debug)
+        flask_cors.CORS(self, origins=self.cfg.app.cors)
+        super().run(self.cfg.app.host, self.cfg.app.port, debug=self.cfg.app.debug)
