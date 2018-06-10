bed2d47b36f9b07bfb8b9eb30aba26c2b5b7588e
Patrick Auernig <patrick.auernig@mykolab.com>
2018-06-10 22:52:20 +0200

    Implement sigma/version endpoint

--- app/controllers/sigma/version_controller.rb
+++ app/controllers/sigma/version_controller.rb
@@ -1,7 +1,23 @@
 module Sigma
   class VersionController < ApplicationController
+    VersionError = Class.new(StandardError)
+
+    rescue_from VersionError do |e|
+      render json: { error: e.message }, status: 500
+    end
+
     def show
-      render json: {}
+      render json: version
+    end
+
+  private
+
+    def version
+      yml = Rails.configuration.x.sigma_path.join("info/version.yml")
+      raise VersionError, "Version filed not found" unless yml.exist?
+      YAML.safe_load(yml.read)
+    rescue SystemCallError, YAML::Exception
+      raise VersionError, "Failed to read version file"
     end
   end
 end
