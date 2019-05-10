23e883bc4daab143c72b313f4bf6846dd648a52e
Patrick Auernig <patrick.auernig@mykolab.com>
2019-05-10 17:13:45 +0200

    Fix member_count statistic accumulation

--- app/models/sigma/stats/general_statistic.rb
+++ app/models/sigma/stats/general_statistic.rb
@@ -12,14 +12,15 @@ class GeneralStatistic
   field :channel_count, type: Integer
   field :member_count, type: Integer
 
+  # rubocop:disable Style/MultilineBlockChain
   def self.collect_stats
-    tmp = where(name: :population)
-          .each_with_object(guild_count: 0, channel_count: 0, member_count: 0) do |item, acc|
-            acc[:guild_count] += item.guild_count if item.guild_count
-            acc[:channel_count] += item.channel_count if item.channel_count
-            acc[:member_count] + item.member_count if item.member_count
-          end
-
-    OpenStruct.new(tmp)
+    where(name: :population)
+      .each_with_object(guild_count: 0, channel_count: 0, member_count: 0) do |item, acc|
+        acc[:guild_count] += item.guild_count if item.guild_count
+        acc[:channel_count] += item.channel_count if item.channel_count
+        acc[:member_count] += item.member_count if item.member_count
+      end
+      .then { |obj| OpenStruct.new(obj) }
   end
+  # rubocop:enable Style/MultilineBlockChain
 end
