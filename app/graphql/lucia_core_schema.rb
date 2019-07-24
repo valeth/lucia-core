1c13ec37e11ac9150243f371cb17c2877bd5b2c1
Patrick Auernig <patrick.auernig@mykolab.com>
2019-07-24 18:36:15 +0200

    GraphQL: Log query complexity and depth

--- app/graphql/lucia_core_schema.rb
+++ app/graphql/lucia_core_schema.rb
@@ -1,4 +1,17 @@
 class LuciaCoreSchema < GraphQL::Schema
+  if Rails.env.development?
+    query_depth_logger = GraphQL::Analysis::QueryDepth.new do |_query, depth|
+      Rails.logger.debug { "GraphQL | Query Depth = #{depth}" }
+    end
+
+    query_complexity_logger = GraphQL::Analysis::QueryComplexity.new do |_query, complexity|
+      Rails.logger.debug { "GraphQL | Query Complexity = #{complexity}" }
+    end
+
+    query_analyzer(query_complexity_logger)
+    query_analyzer(query_depth_logger)
+  end
+
   mutation Types::MutationType
   query Types::QueryType
 end
