839a0521feba5a4b5297bce20dc9db99b164903e
Patrick Auernig <patrick.auernig@mykolab.com>
2019-10-19 19:22:07 +0200

    GraphQL: Add origins field to leaderboard

--- app/graphql/types/sigma/leaderboard_entry_type.rb
+++ app/graphql/types/sigma/leaderboard_entry_type.rb
@@ -9,5 +9,8 @@ module Types::Sigma
     field :current_level, Integer, null: false
     field :points_to_next_level, Integer, null: false, method: :next_level_required
     field :percent_to_next_level, Float, null: false, method: :next_level_percent
+
+    field :origins, Types::Sigma::LeaderboardOriginType, null: false
+    # field :expenses, Types::Sigma::LeaderboardExpensesType, null: true
   end
 end
---
+++ app/graphql/types/sigma/leaderboard_origin_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types::Sigma
+  class LeaderboardOriginType < Types::BaseObject
+    # FIXME: There is no Map type in GraphQL (yet?), so we are forced to use raw JSON here.
+    field :users, GraphQL::Types::JSON, null: false
+    field :guilds, GraphQL::Types::JSON, null: false
+    field :channels, GraphQL::Types::JSON, null: false
+    field :functions, GraphQL::Types::JSON, null: true
+  end
+end
