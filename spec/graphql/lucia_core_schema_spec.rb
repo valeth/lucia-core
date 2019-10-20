99de688a97343f57ecff2181a8960c7653963ca3
Patrick Auernig <patrick.auernig@mykolab.com>
2019-10-20 14:18:56 +0200

    GraphQL: Update test cases for queries

--- spec/graphql/lucia_core_schema_spec.rb
+++ spec/graphql/lucia_core_schema_spec.rb
@@ -3,77 +3,147 @@
 require "rails_helper"
 
 RSpec.describe ::LuciaCoreSchema do
-  describe ".execute" do
-    it "can query bot information" do
-      result = described_class.execute <<~GRAPHQL
-        query {
-          sigma {
-            info {
-              beta
-              buildDate
-              codename
-              version { major minor patch }
+  context "query" do
+    describe "donors" do
+      it "can query all names of donors" do
+        result = described_class.execute <<~GRAPHQL
+          query {
+            donors { name }
+          }
+        GRAPHQL
+
+        expect(result["data"])
+          .to include(
+            "donors" => all(include(
+              "name" => a_string_matching(/^Test\d+$/)
+            ))
+          )
+      end
+    end
+
+    describe "sigma" do
+      it "can query all bot information" do
+        result = described_class.execute <<~GRAPHQL
+          query {
+            sigma {
+              info {
+                beta
+                buildDate
+                codename
+                version { major minor patch }
+              }
             }
           }
-        }
-      GRAPHQL
+        GRAPHQL
 
-      expect(result["data"])
-        .to include(
-          "sigma" => include(
-            "info" => include(
-              "beta" => true,
-              "buildDate" => be_an(Integer),
-              "codename" => be_a(String),
-              "version" => include(
-                "major" => be_an(Integer),
-                "minor" => be_an(Integer),
-                "patch" => be_an(Integer)
+        expect(result["data"])
+          .to include(
+            "sigma" => include(
+              "info" => include(
+                "beta" => true,
+                "buildDate" => be_an(Integer),
+                "codename" => be_a(String),
+                "version" => include(
+                  "major" => be_an(Integer),
+                  "minor" => be_an(Integer),
+                  "patch" => be_an(Integer)
+                )
               )
             )
           )
-        )
-    end
+      end
 
-    it "can query bot modules and commands" do
-      result = described_class.execute <<~GRAPHQL
-        query {
-          sigma {
-            commandCategories {
-              name
-              commands { name }
+      it "can query all command categories with commands" do
+        result = described_class.execute <<~GRAPHQL
+          query {
+            sigma {
+              commandCategories {
+                name
+                commands { name }
+              }
             }
           }
-        }
-      GRAPHQL
+        GRAPHQL
 
-      expect(result["data"])
-        .to include(
-          "sigma" => include(
-            "commandCategories" => all(include(
-              "name" => be_a(String),
-              "commands" => all(include(
-                "name" => be_a(String)
+        expect(result["data"])
+          .to include(
+            "sigma" => include(
+              "commandCategories" => all(include(
+                "name" => be_a(String),
+                "commands" => all(include(
+                  "name" => be_a(String)
+                ))
               ))
-            ))
+            )
           )
-        )
-    end
+      end
 
-    it "can query names of donors" do
-      query = <<~GRAPHQL
-        query {
-          donors { name }
-        }
-      GRAPHQL
+      it "can query all leaderboards" do
+        result = described_class.execute <<~GRAPHQL
+          query {
+            sigma {
+              cookies: leaderboard(type: "COOKIES") {
+                ...leaderboardItems
+              }
+              currency: leaderboard(type: "CURRENCY") {
+                ...leaderboardItems
+              }
+            }
+          }
 
-      result = described_class.execute(query)
-      expect(result["data"])
-        .to include(
-          "donors" => all(include(
-            "name" => a_string_matching(/^Test\d+$/)
-          ))
-        )
+          fragment leaderboardItems on LeaderboardEntry {
+            currentLevel
+            tier
+          }
+        GRAPHQL
+
+        expect(result["data"])
+          .to include(
+            "sigma" => include(
+              "cookies" => all(include(
+                "tier" => be_an(Integer),
+                "currentLevel" => be_an(Integer)
+              )),
+              "currency" => all(include(
+                "tier" => be_an(Integer),
+                "currentLevel" => be_an(Integer)
+              ))
+            )
+          )
+      end
+
+      it "can query all statistics" do
+        result = described_class.execute <<~GRAPHQL
+          query {
+            sigma {
+              stats {
+                commands { name count }
+                events { name count }
+                special { name count }
+                commandCount
+                guildCount
+                channelCount
+                memberCount
+              }
+            }
+          }
+        GRAPHQL
+
+        expect(result["data"])
+          .to include(
+            "sigma" => include(
+              "stats" => include(
+                "commands" => all(include("name" => be_a(String), "count" => be_a(Integer))),
+                "events" => all(include("name" => be_a(String), "count" => be_a(Integer))),
+                "special" => all(include("name" => be_a(String), "count" => be_a(Integer))),
+                "commandCount" => be_a(Integer),
+                "guildCount" => be_a(Integer),
+                "channelCount" => be_a(Integer),
+                "memberCount" => be_a(Integer),
+              )
+            )
+          )
+      end
     end
   end
 end
