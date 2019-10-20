ed11555040a4b5acf3967fe0d7adb4dbaac6f698
Patrick Auernig <patrick.auernig@mykolab.com>
2019-10-20 17:50:13 +0200

    Auth: Implement token authentication for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::API
   include ActionController::Caching
+
+  before_action :authenticate
+
+private
+
+  def authenticate
+    token = request.headers["X-Lucia-Token"]
+
+    if token.nil? || token.empty?
+      render status: 401
+    else
+      validator = LuciaToken::FormatValidator.new(prefix: "lc-")
+      render status: 403 unless validator.valid?(token)
+    end
+  end
 end
