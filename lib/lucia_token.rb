428321847e747ce776570b6d515106d2e9bd914b
Patrick Auernig <patrick.auernig@mykolab.com>
2019-10-23 17:45:22 +0200

    Refine token tests and add error messages

--- lib/lucia_token.rb
+++ lib/lucia_token.rb
@@ -3,9 +3,42 @@
 module LuciaToken
   Error = Class.new(StandardError)
   ValidationError = Class.new(Error)
-  InvalidPrefix = Class.new(ValidationError)
-  InvalidTokenLength = Class.new(ValidationError)
-  InvalidDivider = Class.new(ValidationError)
+
+  class InvalidPrefix < ValidationError
+    def initialize(token, *args)
+      super(%{Token "#{token} has invalid prefix}, *args)
+    end
+  end
+
+  class InvalidTokenLength < ValidationError
+    def initialize(len, *args)
+      super("Invalid token length #{len}", *args)
+    end
+  end
+
+  class InvalidDivider < ValidationError
+    def initialize(idx, *args)
+      super("Invalid divider found at position #{idx}", *args)
+    end
+  end
+
+  class TokenIsEmpty < ValidationError
+    def initialize(*args)
+      super("Token was empty", *args)
+    end
+  end
+
+  class TokenAlreadyUsed < ValidationError
+    def initialize(*args)
+      super("Token was already used", *args)
+    end
+  end
+
+  class InvalidResult < ValidationError
+    def initialize(*args)
+      super("Token yielded invalid result")
+    end
+  end
 
   class FormatValidator
     DIVIDERS = [1, 3, 5, 7, 11, 13].freeze
@@ -31,7 +64,8 @@ module LuciaToken
     # @param token [String]  The full token
     # @raise ValidationError  If any validation step fails
     def validate(token)
-      raise InvalidPrefix unless token.start_with?(@prefix)
+      raise TokenIsEmpty if token.blank?
+      raise InvalidPrefix, token unless token.start_with?(@prefix)
 
       validate_token(token[@prefix.size..])
     end
@@ -43,19 +77,19 @@ module LuciaToken
     # @param token [String] The un-prefixed token string
     # @raise ValidationError  If any validation step fails
     def validate_token(token)
-      raise InvalidTokenLength unless token.size == REQUIRED_LENGTH
+      raise InvalidTokenLength, token.size unless token.size == REQUIRED_LENGTH
 
       total = token.each_char.with_index.reduce(0) do |acc, (char, idx)|
         hex = char.hex
         case idx
-        when 10, 21 then raise InvalidTokenLength unless DIVIDERS.include?(hex)
+        when 10, 21 then raise InvalidDivider, idx unless DIVIDERS.include?(hex)
         else acc += hex
         end
 
         acc
       end
 
-      raise ValidationError unless (total % 10) == 8
+      raise InvalidResult unless (total % 10) == 8
     end
   end
 end
--- spec/lib/lucia_token_spec.rb
+++ spec/lib/lucia_token_spec.rb
@@ -5,21 +5,40 @@ require "rails_helper"
 RSpec.describe LuciaToken::FormatValidator do
   let(:valid_token) { "lc-cd40471e85312c278a6a17b9bd53a388" }
   let(:invalid_token) { "lc-a050aeda033d257d34be239bcd810040" }
+  let(:validator) { described_class.new(prefix: "lc-") }
 
   it "can check for token validity" do
-    validator = described_class.new(prefix: "lc-")
-
     expect(validator.valid?(valid_token)).to eq(true)
     expect(validator.valid?(invalid_token)).to eq(false)
   end
 
   it "raises an exception on invalid tokens" do
-    validator = described_class.new(prefix: "lc-")
-
     expect { validator.validate(valid_token) }
       .not_to raise_error
 
+    expect { validator.validate("") }
+      .to raise_error(LuciaToken::TokenIsEmpty)
+
+    expect { validator.validate(nil) }
+      .to raise_error(LuciaToken::TokenIsEmpty)
+
+    expect { validator.validate(valid_token.tr("lc-", "no-")) }
+      .to raise_error(LuciaToken::InvalidPrefix)
+
+    expect { validator.validate(valid_token[0..-2]) }
+      .to raise_error(LuciaToken::InvalidTokenLength)
+
+    invalid_divider = valid_token.dup
+    invalid_divider[13] = "4"
+    expect { validator.validate(invalid_divider) }
+      .to raise_error(LuciaToken::InvalidDivider)
+
+    invalid_divider = valid_token.dup
+    invalid_divider[24] = "4"
+    expect { validator.validate(invalid_divider) }
+      .to raise_error(LuciaToken::InvalidDivider)
+
     expect { validator.validate(invalid_token) }
-      .to raise_error(LuciaToken::ValidationError)
+      .to raise_error(LuciaToken::InvalidResult)
   end
 end
