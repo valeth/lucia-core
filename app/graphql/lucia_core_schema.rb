54d3446862737d16c9f53bad5b916b37b2d86a12
Patrick Auernig <patrick.auernig@mykolab.com>
2024-08-18 00:55:11 +0200

    Remove graphql query analyzer

--- app/graphql/lucia_core_schema.rb
+++ app/graphql/lucia_core_schema.rb
@@ -3,12 +3,6 @@ class LuciaCoreSchema < GraphQL::Schema
     Logging.logger["GraphQL"]
   end
 
-  analyzer = QueryAnalyzer.new do |_query, depth, complexity|
-    logger.debug { "query_depth=#{depth} query_complexity=#{complexity}" }
-  end
-
-  query_analyzer(analyzer)
-
   disable_introspection_entry_points if Rails.env.production?
 
   query Types::QueryType
